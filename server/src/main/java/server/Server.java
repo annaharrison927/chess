3f69fa317cc49661a984151240b8828880d07579
atupp <atupper@byu.edu>
2025-10-10 15:25:24 -0600

    Updated create, delete, and post methods within the Server constructor

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,9 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+import java.util.Map;
 
 public class Server {
 
@@ -9,10 +12,22 @@ public class Server {
     public Server() {
         server = Javalin.create(config -> config.staticFiles.add("web"));
 
+        // These are just hard coded values!!
         server.delete("db", ctx -> ctx.result("{}"));
+        server.post("user", this::register);
 
         // Register your endpoints and exception handlers here.
 
+    }
+
+    private void register(Context ctx){
+        var serializer = new Gson();
+        var req = serializer.fromJson(ctx.body(), Map.class);
+        req.put("authToken", "cow");
+        var res = serializer.toJson(req);
+        serializer.fromJson(ctx.body(), Map.class);
+        ctx.body();
+        ctx.result("\"username\":\"joe\", \"authToken\":\"xyz\"}");
     }
 
     public int run(int desiredPort) {
