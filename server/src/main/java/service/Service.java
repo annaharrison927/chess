974730e73d4046fe413c81f004c492723f76194d
atupp <atupper@byu.edu>
2025-10-17 16:17:48 -0600

    Updated register method to add a new authToken to the database and create a register result if the user in the request is not already in the database. Still need to add code for throwing Already Taken Exception

--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -1,24 +1,45 @@
 package service;
 
+import dataaccess.AuthDataAccess;
+import dataaccess.MemoryAuthDataAccess;
 import dataaccess.MemoryUserDataAccess;
 import record.RegisterResult;
 import request.RegisterRequest;
 import dataaccess.UserDataAccess;
 import model.UserData;
+import model.AuthData;
+
+import java.util.UUID;
 
 public class Service {
     private final UserDataAccess userDataAccess;
+    private final AuthDataAccess authDataAccess;
 
 
     public Service() {
         userDataAccess = new MemoryUserDataAccess();
+        authDataAccess = new MemoryAuthDataAccess();
     }
 
     public RegisterResult register(RegisterRequest registerRequest) {
+        // Create new user data
         UserData newUser = new UserData(registerRequest.username(), registerRequest.password(), registerRequest.email());
+        // Check if user is already in the database. If not, add the new user to database
         if (userDataAccess.getUser(newUser) != null) {
+            // Throw Already Taken Exception
         }
         userDataAccess.addUser(newUser);
 
+        // Generate new authToken and add to database
+        String authToken = generateToken();
+        AuthData newAuth = new AuthData(authToken, newUser.username());
+        authDataAccess.addAuth(newAuth);
+
+        // Create register result
+        return new RegisterResult(newUser.username(), authToken);
+    }
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
     }
 }
