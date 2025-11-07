dbf72314187050f675dd4f5773e95cdd78c0021e
annaharrison927 <atupper927@gmail.com>
2025-11-06 19:55:49 -0700

    Created a register method. Updated run and evaluate methods

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -1,5 +1,7 @@
 package ui;
 
+import model.UserData;
+
 import javax.swing.plaf.nimbus.State;
 
 import java.util.Arrays;
@@ -21,23 +23,48 @@ public class Client {
 
         Scanner scanner = new Scanner(System.in);
         var result = "";
-        while (!result.equals("quit")){
+        while (!result.equals("quit")) {
             // PRINT PROMPT??
             String line = scanner.nextLine();
 
+            try {
+                result = evaluate(line);
+                System.out.print(result);
+            } catch (Throwable e) {
+                String message = e.toString();
+                System.out.print(message);
+            }
         }
+        System.out.println();
     }
 
-    public String evaluate (String input){
-        try{
+    public String evaluate(String input) {
+        try {
             String[] tokens = input.toLowerCase().split(" ");
             String command = "help";
-            if (tokens.length > 0){
+            if (tokens.length > 0) {
                 command = tokens[0];
             }
             String[] parameters = Arrays.copyOfRange(tokens, 1, tokens.length);
 
+            return switch (command) {
+                case "register" -> register(parameters);
+                default -> help();
+            };
+        } catch (Exception ex) {
+            return ex.getMessage();
         }
+    }
+
+    public String register(String... params) throws Exception {
+        String username = params[0];
+        String password = params[1];
+        String email = params[2];
+
+        UserData userData = new UserData(username, password, email);
+        serverFacade.register(userData);
+
+        return String.format("Hi %s, you've successfully registered!", username);
     }
 
     public String help() {
